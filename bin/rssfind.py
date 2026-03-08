e1418d7e5f16ead5e3ea7a7db81f2f6d81c16ecf
Alexandre Dulaunoy <a@foo.be>
2026-03-08 17:14:21 +0000

    Resolve relative Atom alternate links in rssfind

--- bin/rssfind.py
+++ bin/rssfind.py
@@ -53,17 +53,29 @@ def findfeeds(url=None, disable_strict=False):
         for f in feed_urls:
             tag = f.get("type", None)
             if tag:
-                if "feed" in tag or "rss" in tag or "xml" in tag:
+                tag = tag.lower()
+                if (
+                    "feed" in tag
+                    or "rss" in tag
+                    or "atom" in tag
+                    or "xml" in tag
+                ):
                     href = f.get("href", None)
                     if href:
-                        discovered_feeds.append(href)
+                        discovered_feeds.append(urllib.parse.urljoin(url, href))
 
     ahreftags = html.findAll("a")
 
     for a in ahreftags:
         href = a.get("href", None)
         if href:
-            if "feed" in href or "rss" in href or "xml" in href:
+            href_lower = href.lower()
+            if (
+                "feed" in href_lower
+                or "rss" in href_lower
+                or "atom" in href_lower
+                or "xml" in href_lower
+            ):
                 discovered_feeds.append(urllib.parse.urljoin(url, href))
 
     for url in list(set(discovered_feeds)):
